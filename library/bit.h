7a0b33de9a4f5ac81d37bd0237a1b80bc0fcaf39
superuzir <jz@bk.ru>
2019-04-30 11:54:29 +0700

    Split library into lib+iter+bit

---
+++ library/bit.h
@@ -0,0 +1,2 @@
+static ll nextPowerOf2(ll n){ ll count = 0; if (n && !(n & (n - 1))) return n; while(n) { n >>= 1ll; count++; } return 1ll << count; }
+static ll prevPowerOf2(ll n){ for (ll i=n; i>=1; i--) if ((i & (i-1)) == 0) return i; return 0; }
--- library/lib.hpp
+++ library/lib.hpp
@@ -21,8 +21,6 @@ struct WhiteSpacedCout { bool space; std::ostream& out; explicit WhiteSpacedCout
 template<typename T1, typename T2> std::ostream &operator<<(std::ostream &s, const std::pair<T1,T2> &x){ s << "pair(" << x.first << " ," << x.second << ')'; return s; }
 template<typename T> std::ostream &operator<<(std::ostream &s, const std::vector<T> &x){ if(x.empty()){ s << "vector(empty)"; return s;} s << "vector("; for(size_t i = 0; i < x.size() - 1; i++){ s << x[i] << ", "; } s << x[x.size() - 1] << ')'; return s; }
 
-template<class ForwardIt, class T> ForwardIt binsearch(ForwardIt first, ForwardIt last, const T& value){ first = std::lower_bound(first, last, value); if(first == last) return last;	return *first == value ? first : last; }
-
 typedef long long ll;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
@@ -33,7 +31,6 @@ typedef vector<pll> vpll;
 template<typename T> static vector<T> vin(ll n, std::istream& in){ vector<T> ret; forn(i, n){ T val; in >> val; ret.pb(val); } return ret; }
 template<typename T> static T read(std::istream& in){ T val; in >> val; return val; }
 template<typename T> void answer(const T & cont, std::ostream & out){ for(auto & v : cont){ out << v << ' '; }; out << '\n'; }
-template <typename It, typename T = typename It::value_type, typename Pr = typename std::function<void(It, It)>, typename Eq = typename std::function<bool(T, T)> >void itersame(It begin, It end, Pr process, Eq equal = std::equal_to<T>()){for (auto b = begin, e = begin; e != end;){e = find_if(b, end, [&](T pr) { return !equal(pr, *b); }); process(b, e); b = e; }}
 
 #define read_vll(n) vin<ll>(n, in)
 #define read_vs(n)  vin<string>(n, in)
